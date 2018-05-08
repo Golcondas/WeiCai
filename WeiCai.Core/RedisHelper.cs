f5abd0ddfeb5a4a12c753b60abd5fd9484d38555
nibaoshan@zillionfortune.com <nibaoshan@zillionfortune.com>
2018-05-08 14:29:22 +0800

    #add redis 缓存

---
+++ WeiCai.Core/RedisHelper.cs
@@ -0,0 +1,114 @@
+﻿/******************************* 
+** 作者： nibaoshan 
+** 时间： 2018/5/8 14:06:39 
+** 版本: V1.0.0 
+** CLR: 4.0.30319.42000 
+** GUID: 340d5596-15ae-4836-96b7-72a091ad9a8f 
+** 描述： 尚未编写描述 
+*******************************/
+
+using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeiCai.Core
+{
+
+    public class RedisHelper
+    {
+        #region static field
+
+        static string redisIp = ConfigurationManager.AppSettings["redisIp"].ToString();
+        static string redisPort = ConfigurationManager.AppSettings["redisPort"].ToString();
+        static string redisPassword = ConfigurationManager.AppSettings["redisPassword"].ToString();
+        static RedisClient client = new RedisClient(redisIp, Convert.ToInt32(redisPort));
+        static RedisHelper()
+        {
+            client.Password = redisPassword;
+        }
+
+        #endregion
+        #region
+        /// <summary>
+        /// 获取信息
+        /// </summary>
+        /// <typeparam name="T">对象类型</typeparam>
+        /// <returns>对象</returns>
+        public static T Get<T>(string token) where T : class, new()
+        {
+            return client.Get<T>(token);
+        }
+
+        /// <summary>
+        /// 获取信息
+        /// </summary>
+        /// <typeparam name="T">对象类型</typeparam>
+        /// <returns>对象</returns>
+        public static String GetString(string token)
+        {
+            return client.Get<String>(token);
+        }
+        /// <summary>
+        /// 设置信息
+        /// </summary>
+        /// <typeparam name="T">对象类型</typeparam>
+        /// <param name="token">key</param>
+        /// <param name="obj">对象</param>
+        public static bool Set<T>(string token, T obj) where T : class, new()
+        {
+            return client.Set<T>(token, obj);
+        }
+
+        /// <summary>
+        /// 设置信息
+        /// </summary>
+        /// <typeparam name="T">对象类型</typeparam>
+        /// <param name="token">key</param>
+        /// <param name="obj">对象</param>
+        public static bool SetString(string token, string value)
+        {
+            return client.Set<String>(token, value);
+        }
+
+        public static bool SetTime<T>(string token, DateTime dt, T obj) where T : class, new()
+        {
+            return client.Set<T>(token, obj, dt);
+        }
+
+        /// <summary>
+        /// 设置信息
+        /// </summary>
+        /// <typeparam name="T">对象类型</typeparam>
+        /// <param name="token">key</param>
+        /// <param name="obj">对象</param>
+        public static bool SetStringTime(string token, string value, DateTime dt)
+        {
+            return client.Set<String>(token, value, dt);
+        }
+
+        /// <summary>
+        /// 删除缓存
+        /// </summary>
+        /// <param name="keys"></param>
+        /// <returns></returns>
+        public static int DelKey(params string[] keys)
+        {
+            return client.Del(keys);
+        }
+
+        /// <summary>
+        /// 设置指定Key的过期时间
+        /// </summary>
+        /// <param name="token">具体的key值</param>
+        /// <param name="seconds">过期时间，单位：秒</param>
+        public static int Expire(string token, int seconds)
+        {
+            return client.Expire(token, seconds);
+        }
+        #endregion
+    }
+}
---
+++ WeiCai.Test/RedisHelperTest.cs
@@ -0,0 +1,36 @@
+﻿/******************************* 
+** 作者： nibaoshan 
+** 时间： 2018/5/8 14:18:58 
+** 版本: V1.0.0 
+** CLR: 4.0.30319.42000 
+** GUID: 97581d5d-4f6d-4ff4-9b19-7e9133f5c7e0 
+** 描述： 尚未编写描述 
+*******************************/
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeiCai.Core;
+
+namespace WeiCai.Test
+{
+    [TestClass]
+    public class RedisHelperTest
+    {
+        [TestMethod]
+        public void TestGetRedis()
+        {
+            //var keyA = RedisHelper.GetString("a");
+            //var keyB = RedisHelper.SetString("a","宝山");
+            for (int i = 1; i < 10000; i++)
+            {
+                string key = i + "";
+                var keyC = RedisHelper.SetStringTime(key, "宝山", DateTime.Now.AddMinutes(1));
+            }
+
+        }
+    }
+}
