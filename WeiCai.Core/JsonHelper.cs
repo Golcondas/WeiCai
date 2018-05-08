12369c47eae0af39691c251b4eebf40bad64ef8d
nibaoshan@zillionfortune.com <nibaoshan@zillionfortune.com>
2018-05-08 14:59:25 +0800

    #update 搭建基础架构

---
+++ WeiCai.Core/JsonHelper.cs
@@ -0,0 +1,38 @@
+﻿/******************************* 
+** 作者： nibaoshan 
+** 时间： 2018/5/8 14:35:20 
+** 版本: V1.0.0 
+** CLR: 4.0.30319.42000 
+** GUID: d988a1fe-2b06-490b-bbfd-76726aa5cb03 
+** 描述： 尚未编写描述 
+*******************************/
+
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeiCai.Core
+{
+    class JsonHelper
+    {
+        public static string ObjectToJson(object obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+        // 从一个Json串生成对象信息
+        public static T JsonToObject<T>(string jsonString)
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject<T>(jsonString);
+            }
+            catch (Exception ex)
+            {
+                return default(T);
+            }
+        }
+    }
+}
---
+++ WeiCai.ManageentWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WeiCai.ManageentWeb.MvcApplication" Language="C#" %>
